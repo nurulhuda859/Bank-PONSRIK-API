8da556234963d3a6e130635391c8b174bf303e3c
Nurul Huda <nurulhuda859g@gmail.com>
2019-05-23 13:52:01 +0600

    cleaned up server rendering

--- utils/customergetter.js
+++
@@ -1,9 +0,0 @@
-const { Customer } = require('../models/customer');
-const { Account, validate } = require('../models/account');
-const { Deposit } = require('../models/deposit');
-
-exports.get = async (id) => {
-    const customer = await Customer.find({id}).populate('accounts');
-    const deposit = await Deposit.find({acn: customer[0].accounts[0].acn});
-    return {customer, deposit}
-}
